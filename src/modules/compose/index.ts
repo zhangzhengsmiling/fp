4c16192f8f0eacc154d54ab3762a2bf688ebf631
zhangzhengsmiling <40157660+zhangzhengsmiling@users.noreply.github.com>
2022-02-22 15:12:25 +0800

    Revert "Feature/libs"

--- src/modules/compose/index.ts
+++
@@ -1,8 +0,0 @@
-
-const compose = (...fns: Function[]) => {
-  return (trigger: any) => {
-    return fns.reduceRight((temp, fn) => fn(temp), trigger)
-  }
-}
-
-export default compose;
--- src/modules/map/index.ts
+++
@@ -1,3 +0,0 @@
-import curry from '../curry';
-const map = (fn: (v: any) => any, array: any[]) => array.map(fn)
-export default curry(map);
