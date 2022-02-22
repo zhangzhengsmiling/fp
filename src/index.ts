e2ff23890e50a81edd1fa601e42aa7e39b68c486
yun_yang <yun_yang@hunliji.com>
2022-02-22 14:51:10 +0800

    feat: functional libs

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as curry } from './modules/curry';
--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as gcd } from './modules/gcd'
-export { default as lcm } from './modules/lcm'
-export { default as Loading } from './modules/Loading'
