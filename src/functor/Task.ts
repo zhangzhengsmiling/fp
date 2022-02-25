a00d3761c9158aafef3a40ae329c3d30a80bad16
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-02-25 15:33:28 +0800

    feat: add task functor

---
+++ src/functor/Task.ts
@@ -0,0 +1,59 @@
+// import { compose } from '../modules'
+// import { FunctionType } from '../types';
+// import { identity } from '../combinator';
+
+type FunctionType = (...args: unknown[]) => unknown;
+
+const compose = (...fns: FunctionType[]) => {
+  return (trigger: any) => {
+    return fns.reduceRight((temp, fn) => fn(temp), trigger);
+  };
+};
+
+const identity = <T = any>(v: T) => v;
+
+class Task {
+  __value: any;
+  resolve: any = identity;
+  reject: any = identity;
+  constructor(task: FunctionType) {
+    this.__value = task;
+  }
+
+  static of (task: any) {
+    return new Task(task);
+  }
+
+  map(f: FunctionType) {
+    this.resolve = compose(f, this.resolve);
+    return this;
+  }
+
+  fail(f: FunctionType) {
+    this.reject = compose(f, this.reject);
+    return this;
+  }
+
+  execute() {
+    return this.__value(this.resolve, this.reject)
+  }
+}
+
+const task = Task.of((resolve: any, reject:any) => {
+  console.log('start loading...')
+  setTimeout(() => {
+    return resolve(1234)
+  }, 1234)
+})
+.map((v: any) => {
+  console.log(v)
+  console.log('end loading....')
+  return v;
+})
+.fail((e: any) => {
+  console.log('error', e)
+})
+
+
+
+console.log(task.execute())
